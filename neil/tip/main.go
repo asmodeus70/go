a19a2e68c521764a0b897328a9793d6c2cae841a
Neil Radley <nradley@gmail.com>
2017-05-08 12:32:42 +0100

    Added £ sign to the output

--- neil/tip/main.go
+++ neil/tip/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 )
 
 func main() {
@@ -16,9 +17,13 @@ func main() {
 
 	// Now we calculate the tip
 	fmt.Println("\nThe tip is")
+	color.Set(color.FgGreen)
 	var totaltip float32 = (bill * tip / 100)
 	var billtotal float32 = (totaltip + bill)
 	fmt.Printf("£%.2f \n", +totaltip)
+	color.Set(color.FgWhite)
 	fmt.Println("\nThe total bill is")
+	color.Set(color.FgRed)
 	fmt.Printf("£%.2f \n", +billtotal)
+	color.Set(color.FgWhite)
 }
